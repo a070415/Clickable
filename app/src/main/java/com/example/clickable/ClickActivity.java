3142914aadc9834ad8c257603684ba1037d6e245
jhjo <jhjo@finedigital.com>
2023-01-21 11:38:17 +0900

    설명 다이얼로그 추가, 다이얼로그 비율 조절

--- app/src/main/java/com/example/clickable/ClickActivity.java
+++ app/src/main/java/com/example/clickable/ClickActivity.java
@@ -2,9 +2,14 @@ package com.example.clickable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Display;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,6 +17,8 @@ public class ClickActivity extends AppCompatActivity {
 
     private TextView mtvTimer, mtvCount;
     private LinearLayout mllTabArea;
+
+    private Dialog dialog;
 
     private int count = 0;
 
@@ -37,8 +44,13 @@ public class ClickActivity extends AppCompatActivity {
             }
         });
 
+        // 다이얼로그
+        dialog = new Dialog(ClickActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog);
+
         // 타이머
-        CountDownTimer countDownTimer = new CountDownTimer(15000, 1000) {
+        CountDownTimer countDownTimer = new CountDownTimer(3000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 int num = (int) (millisUntilFinished / 1000);
@@ -49,7 +61,42 @@ public class ClickActivity extends AppCompatActivity {
             public void onFinish() {
                 mtvTimer.setText("끝");
                 mllTabArea.setClickable(false);
+                showDialog();
             }
         }.start();
+    }
+
+    public void showDialog() {
+        dialog.show();
+        dialog.setCancelable(false); //  다이얼로그 외부 터치 금지
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        Window window = dialog.getWindow();
+
+        int x = (int)(size.x * 0.7f);
+        int y = (int)(size.y * 0.2f);
+
+        window.setLayout(x,y);
+
+//        // no 버튼을 눌렀을때
+//        Button noBtn = dialog.findViewById(R.id.noBtn);
+//        noBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                dialog.dismiss();
+//            }
+//        });
+
+        // yes 버튼을 눌렀을때
+        Button mBtnRetry = dialog.findViewById(R.id.btn_retry);
+        mBtnRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/clickable/MainActivity.java
+++ app/src/main/java/com/example/clickable/MainActivity.java
@@ -2,23 +2,32 @@ package com.example.clickable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Dialog mInfoDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mInfoDialog = new Dialog(MainActivity.this);
+        mInfoDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        mInfoDialog.setContentView(R.layout.info_dialog);
+
         findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ClickActivity.class);
-                gotoActivity(intent);
+                showDialog();
             }
         });
     }
@@ -29,5 +38,31 @@ public class MainActivity extends AppCompatActivity {
         } else {
             startActivity(intent);
         }
+    }
+
+    public void showDialog() {
+        mInfoDialog.show();
+        mInfoDialog.setCancelable(false); //  다이얼로그 외부 터치 금지
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        Window window = mInfoDialog.getWindow();
+
+        int x = (int)(size.x * 0.7f);
+        int y = (int)(size.y * 0.2f);
+
+        window.setLayout(x,y);
+
+        // ok 버튼을 눌렀을때
+        Button mBtnDialogOk = mInfoDialog.findViewById(R.id.btn_dialog_ok);
+        mBtnDialogOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ClickActivity.class);
+                gotoActivity(intent);
+            }
+        });
     }
 }
